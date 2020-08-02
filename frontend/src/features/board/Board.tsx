6b928c952ddced615fe8e3ec52eec8b0c9549742
Eddie Sholl <esholl@zendesk.com>
2020-08-02 22:52:50 +1000

    Simple sort by priority, but can't drag yet

--- frontend/src/features/board/Board.tsx
+++ frontend/src/features/board/Board.tsx
@@ -8,7 +8,7 @@ import {
   DropResult,
 } from "react-beautiful-dnd";
 import Column from "features/column";
-import { IColumn } from "types";
+import { IColumn, ITask, PriorityValue } from "types";
 import reorder, { reorderTasks } from "utils/reorder";
 import { RootState } from "store";
 import { useSelector, useDispatch } from "react-redux";
@@ -52,6 +52,16 @@ const RightMargin = styled.div`
     width: 0.5rem;
   }
 `;
+
+const priorityToSortValue = (p: PriorityValue) =>
+  p == "H" ? 3 : p == "M" ? 2 : 1;
+
+const sortTasks = (ta: ITask, tb: ITask) => {
+  const va = priorityToSortValue(ta.priority);
+  const vb = priorityToSortValue(tb.priority);
+
+  return vb - va;
+};
 
 const Board = () => {
   const detail = useSelector((state: RootState) => state.board.detail);
@@ -134,9 +144,9 @@ const Board = () => {
                     id={column.id}
                     title={column.title}
                     index={index}
-                    tasks={tasksByColumn[column.id].map(
-                      (taskId) => tasksById[taskId]
-                    )}
+                    tasks={tasksByColumn[column.id]
+                      .map((taskId) => tasksById[taskId])
+                      .sort(sortTasks)}
                   />
                 ))}
                 {provided.placeholder}
