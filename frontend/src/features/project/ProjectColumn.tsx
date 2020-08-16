180141f657f77072b7c5fea2f6fbc8bfca3b6e4d
Eddie Sholl <esholl@zendesk.com>
2020-08-16 22:27:34 +1000

    Sort projects by due date then priority

--- frontend/src/features/project/ProjectColumn.tsx
+++ frontend/src/features/project/ProjectColumn.tsx
@@ -2,16 +2,26 @@ import React from "react";
 import styled from "@emotion/styled";
 import { grid } from "const";
 import { COLUMN_COLOR, PRIMARY } from "utils/colors";
-import { IProject } from "types";
+import { IProject, PriorityValue } from "types";
 import {
   Draggable,
   DraggableProvided,
   DraggableStateSnapshot,
 } from "react-beautiful-dnd";
+import differenceInDays from "date-fns/differenceInDays";
 import ColumnTitle from "components/ColumnTitle";
 import ProjectList from "./ProjectList";
 import { useSelector } from "react-redux";
 import { RootState } from "store";
+
+const DUE_DATE_IGNORE_DAYS = 10;
+const projectDueInOrIgnore = (project: IProject) =>
+  project.due_date == null
+    ? DUE_DATE_IGNORE_DAYS
+    : differenceInDays(new Date(project.due_date), new Date());
+
+const priorityToSortValue = (p: PriorityValue) =>
+  p == "H" ? 3 : p == "M" ? 2 : 1;
 
 const Container = styled.div`
   margin: ${grid / 2}px;
@@ -44,13 +54,35 @@ const ProjectColumn = ({ projects }: Props) => {
     (state: RootState) => state.board.filterByLabels
   );
 
-  const filteredProjects = projects.filter(
-    (project) =>
-      filterByLabels.length == 0 ||
-      filterByLabels.some((filterByLabel) =>
-        project.labels.includes(filterByLabel)
-      )
-  );
+  const filteredProjects = projects
+    .filter(
+      (project) =>
+        filterByLabels.length == 0 ||
+        filterByLabels.some((filterByLabel) =>
+          project.labels.includes(filterByLabel)
+        )
+    )
+    .sort((a, b) => {
+      const aDueIn = projectDueInOrIgnore(a);
+      const bDueIn = projectDueInOrIgnore(b);
+
+      if (aDueIn < DUE_DATE_IGNORE_DAYS) {
+        if (bDueIn > aDueIn) {
+          return -1;
+        } else if (bDueIn < aDueIn && bDueIn < DUE_DATE_IGNORE_DAYS) {
+          return 1;
+        }
+      } else if (bDueIn < DUE_DATE_IGNORE_DAYS) {
+        return 1;
+      }
+
+      const priorityA = priorityToSortValue(a.priority);
+      const priorityB = priorityToSortValue(b.priority);
+
+      return priorityB - priorityA;
+
+      // return 0;
+    });
 
   return (
     <Draggable draggableId={`col-${id}`} index={index}>
